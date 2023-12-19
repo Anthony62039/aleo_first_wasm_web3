b109fe09db999bb0c900808b10da49dce9b8d783
Your Name <you@example.com>
2023-12-19 18:09:50 +0000

    first commit, new aleo app

---
+++ src/workers/worker.js
@@ -0,0 +1,72 @@
+import {
+  Account,
+  ProgramManager,
+  PrivateKey,
+  initThreadPool,
+  AleoKeyProvider,
+  AleoNetworkClient,
+  NetworkRecordProvider,
+} from "@aleohq/sdk";
+import { expose, proxy } from "comlink";
+
+await initThreadPool();
+
+async function localProgramExecution(program, aleoFunction, inputs) {
+  const programManager = new ProgramManager();
+
+  // Create a temporary account for the execution of the program
+  const account = new Account();
+  programManager.setAccount(account);
+
+  const executionResponse = await programManager.executeOffline(
+    program,
+    aleoFunction,
+    inputs,
+    false,
+  );
+  return executionResponse.getOutputs();
+}
+
+async function getPrivateKey() {
+  const key = new PrivateKey();
+  return proxy(key);
+}
+
+async function deployProgram(program) {
+  const keyProvider = new AleoKeyProvider();
+  keyProvider.useCache(true);
+
+  // Create a record provider that will be used to find records and transaction data for Aleo programs
+  const networkClient = new AleoNetworkClient("https://vm.aleo.org/api");
+
+  // Use existing account with funds
+  const account = new Account({
+    privateKey: "user1PrivateKey",
+  });
+
+  const recordProvider = new NetworkRecordProvider(account, networkClient);
+
+  // Initialize a program manager to talk to the Aleo network with the configured key and record providers
+  const programManager = new ProgramManager(
+    "https://vm.aleo.org/api",
+    keyProvider,
+    recordProvider,
+  );
+
+  programManager.setAccount(account);
+
+  // Define a fee to pay to deploy the program
+  const fee = 1.9; // 1.9 Aleo credits
+
+  // Deploy the program to the Aleo network
+  // const tx_id = await programManager.deploy(program, fee);
+
+  // Optional: Pass in fee record manually to avoid long scan times
+  const feeRecord = "{\n  program_id: credits.aleo,\n  function_name: transfer_public,\n  arguments: [\n    aleo1frx032074sr7e6zauwdljceycdpcz6t3m96ly2xfpx7g4ejqvypqtjkf2d,\n    aleo1um93p4qdegnmnp74dtdu40je7n8sqf7gqj6w4l5mdf5ahf9qmuqqevzhux,\n    15000000u64\n  ]\n}";
+  const tx_id = await programManager.deploy(program, fee, undefined, feeRecord);
+
+  return tx_id;
+}
+
+const workerMethods = { localProgramExecution, getPrivateKey, deployProgram };
+expose(workerMethods);
